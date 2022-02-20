fc3dc5856b1296ce182245614bb437622ada7fd0
suzanemenon <suzanemenon@gmail.com>
2022-02-20 11:26:45 -0300

    Adding financial advisors data

---
+++ src/utils/financial_advisors_data.js
@@ -0,0 +1,106 @@
+const financialAdvisorData = [
+  {
+    email: 'lilianeb@xp.com',
+    image: '/profile/liliane_bittencourt.png',
+    investments: ['CNPI', 'Renda Variável'],
+    name: 'Liliane Bittencourt',
+  },
+  {
+    email: 'fcastanhari@xp.com',
+    image: '/profile/fabricio_castanhari.png',
+    investments: ['CNPI', 'Renda Variável'],
+    name: 'Fabrício Castanhari',
+  },
+  {
+    email: 'manssour@xp.com',
+    image: '/profile/matheus_manssour.png',
+    investments: ['CNPI', 'Renda Variável', 'CDB'],
+    name: 'Matheus Manssour',
+  },
+  {
+    email: 'lucas.m.ribeiro@xp.com',
+    image: '/profile/lucas_ribeiro.png',
+    investments: ['Ações B3', 'Renda Fixa'],
+    name: 'Lucas Ribeiro',
+  },
+  {
+    email: 'carol.aguiar@xp.com',
+    image: '/profile/carol-aguiar.png',
+    investments: ['Criptomoedas'],
+    name: 'Carol Aguiar',
+  },
+  {
+    email: 'hanna.souza@xp.com',
+    image: '/profile/hanna_souza.png',
+    investments: ['CRA', 'CRI', 'LCA', 'LCI', 'FIIs'],
+    name: 'Hanna Souza',
+  },
+  {
+    email: 'zucco@xp.com',
+    image: '/profile/bruno_zucco.png',
+    investments: ['CNPI', 'Renda Variável'],
+    name: 'Bruno Zucco',
+  },
+    {
+    email: 'david.lee@xp.com',
+    image: '/profile/david_lee.png',
+    investments: ['Criptomoedas'],
+    name: 'David Lee',
+  },
+    {
+    email: 'janaina.mossman@xp.com',
+    image: '/profile/janaina_mossman.png',
+    investments: ['CNPI', 'Renda Variável'],
+    name: 'Janaina Mossman',
+  },
+    {
+    email: 'katerinesilva@xp.com',
+    image: '/profile/katerine_silva.png',
+    investments: ['CRA', 'CRI', 'LCA', 'LCI', 'FIIs'],
+    name: 'Katerine Silva',
+  },
+    {
+    email: 'moaes.liziane@xp.com',
+    image: '/profile/linziane_moraes.png',
+    investments: ['CNPI', 'Renda Variável', 'CDB'],
+    name: 'Liziane Moraes',
+  },
+    {
+    email: 'jacinta.martinho@xp.com',
+    image: '/profile/jacinta_martinho.png',
+    investments: ['CNPI', 'Renda Variável', 'CDB'],
+    name: 'Jacinta Martinho',
+  },
+    {
+    email: 'estevan.dimas@xp.com',
+    image: '/profile/estevan_dimas.png',
+    investments: ['CNPI', 'Renda Variável', 'CDB'],
+    name: 'Estevan Dimas',
+  },
+  {
+    email: 'raquel.anabela@xp.com',
+    image: '/profile/raquel_anabela.png',
+    investments: ['CNPI', 'Renda Variável', 'CDB'],
+    name: 'Raquel Anabela',
+  },
+  {
+    email: 'valente.calixto@xp.com',
+    image: '/profile/valente_calixto.png',
+    investments: ['Criptomoedas'],
+    name: 'Valente Calixto',
+  },
+  {
+    email: 'luiza.doris@xp.com',
+    image: '/profile/luiza_doris.png',
+    investments: ['Ações B3', 'Renda Fixa'],
+    name: 'Luiza Dóris',
+  },
+  {
+    email: 'micaela.cesario@xp.com',
+    image: '/profile/micaela_cesario.png',
+    investments: ['CRA', 'CRI', 'LCA', 'LCI', 'FIIs'],
+    name: 'Micaela Cesário',
+  },
+];
+
+export default financialAdvisorData;
