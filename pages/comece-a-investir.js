d05e9828d9609f075e9890c9168e53450f9c2728
suzanemenon <suzanemenon@gmail.com>
2022-02-20 15:33:52 -0300

    Adding xp values to investing page

--- pages/comece-a-investir.js
+++ pages/comece-a-investir.js
@@ -7,7 +7,8 @@ import {
   Card,
   CardMedia,
   Typography,
-  Link
+  Link,
+  Image
 } from '@mui/material';
 
 import { styled } from '@mui/material/styles';
@@ -30,6 +31,78 @@ export default function XPConectaPage() {
         </Container>
       </Header>
       <Container disableGutters sx={{ pl: 4, pr: 4, pt: '50px', pb: '100px' }}>
+        <Grid container spacing={2} columns={18} sx={{ pb: '100px' }}>
+          <Grid item xs={6}>
+            <Card elevation="none" sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+              alignItems: 'center',
+              p: 1,
+            }}>
+              <CardMedia
+                component="img"
+                image="assets/icon-welcome.png"
+                alt={"Welcome"}
+                sx={{ height: 160, width: 160, mb: 5 }}
+              />
+              <Box>
+                <Typography  sx={{ mb: 3, fontWeight: 'bold' }} component="h1" variant="h5">Pioneiros em assesso</Typography>
+                <Typography component="p" variant="5">
+                  Com a Assessoria XP, ajudamos as pessoas nas decisões relacionadas aos seus investimentos, sempre de acordo com seus objetivos e perfil.
+                </Typography>
+              </Box>
+            </Card>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Card elevation="none" sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+              alignItems: 'center',
+              p: 1,
+            }}>
+              <CardMedia
+                component="img"
+                image="assets/icon-advice.png"
+                alt={"Advice"}
+                sx={{ height: 160, width: 160, mb: 5 }}
+              />
+              <Box>
+                <Typography sx={{ mb: 3, fontWeight: 'bold' }} component="h1" variant="h5">Desafiamos padrões</Typography>
+                <Typography component="p" variant="5">
+                  Nosso time de tecnologia antecipa tendências do mercado, para tornar a experiência de investir mais simples e acessível para todas as pessoas.
+                </Typography>
+              </Box>
+            </Card>            
+          </Grid>
+
+          <Grid item xs={6}>
+            <Card elevation="none" sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+              alignItems: 'center',
+              p: 1,
+            }}>
+              <CardMedia
+                component="img"
+                image="assets/icon-centralized.png"
+                alt={"Centralized"}
+                sx={{ height: 160, width: 160, mb: 5 }}
+              />
+              <Box>
+                <Typography sx={{ mb: 3, fontWeight: 'bold' }} component="h1" variant="h5">Tudo em um lugar</Typography>
+                <Typography component="p" variant="5">
+                  Serviços financeiros a favor dos seus investimentos. Tudo em um só lugar, para quem investe não depender mais dos bancos convencionais.
+                </Typography>
+              </Box>
+            </Card>            
+          </Grid>
+        </Grid>
+        
+        
         <Grid container spacing={2} columns={16}>
           <Grid item xs={6}>
             <Link
