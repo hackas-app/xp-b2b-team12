f1211c15cc49772f448c8e8e42d8590115c8482c
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2022-02-20 20:30:30 -0300

    fix grids

--- src/components/pie_chart.js
+++ src/components/pie_chart.js
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { VictoryPie } from 'victory';
 import { Box, Card, CardMedia, Chip, Typography } from '@mui/material';
 
-export default function PieChart({data, name, logo, earning, theme}) {
+export default function PieChart({ data, name, logo, earning, theme }) {
   const colorPalette = [
-    "#ffc70c",
-    "#f2c661",
-    "#e0c695",
-    "#c6c6c6",
-    "#89898c",
-    "#505155",
-    "#1e1f24"
+    '#ffc70c',
+    '#f2c661',
+    '#e0c695',
+    '#c6c6c6',
+    '#89898c',
+    '#505155',
+    '#1e1f24',
   ];
 
   return (
@@ -26,40 +26,37 @@ export default function PieChart({data, name, logo, earning, theme}) {
       }}
       variant="elevation"
     >
-      <CardMedia
-        component="img"
-        image={logo}
-        height={80}
-        sx={{ mb: 1}}
-      />
-        <Typography 
-          variant="h5"
-          component="h1"
-          sx={{ color: theme.palette.tertiary.main }}>
-          {name}
-        </Typography>
-        <Box sx={{
+      <CardMedia height={80} sx={{ mb: 1 }} />
+      <Typography
+        variant="h5"
+        component="h1"
+        sx={{ color: theme.palette.tertiary.main }}
+      >
+        {name}
+      </Typography>
+      <Box
+        sx={{
           display: 'flex',
           flexDirection: 'column',
           flexGrow: 1,
-        }}>
-          <Box 
-            sx={{
-              mt: 1,
-              ml: '-2px',
-              mr: '-2px',
+        }}
+      >
+        <Box
+          sx={{
+            mt: 1,
+            ml: '-2px',
+            mr: '-2px',
+            mb: '4px',
+
+            '> *': {
+              ml: '2px',
+              mr: '2px',
               mb: '4px',
-              
-              '> *': {
-                ml: '2px',
-                mr: '2px',
-                mb: '4px'
-              },
-            }}
-          >
-          </Box>
-        </Box>
-      <Box 
+            },
+          }}
+        ></Box>
+      </Box>
+      <Box
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
@@ -68,29 +65,30 @@ export default function PieChart({data, name, logo, earning, theme}) {
           mt: 2,
         }}
       >
-        <VictoryPie 
+        <VictoryPie
           data={data}
           colorScale={colorPalette}
           style={{
             data: {
-              fillOpacity: 0.9, stroke: theme.palette.primary.main, strokeWidth: 3
+              fillOpacity: 0.9,
+              stroke: theme.palette.primary.main,
+              strokeWidth: 3,
             },
             labels: {
-              fontSize: 16, fill: "white"
-            }
+              fontSize: 16,
+              fill: 'white',
+            },
           }}
-          labelRadius={({ innerRadius }) => innerRadius + 100 }
+          labelRadius={({ innerRadius }) => innerRadius + 100}
         />
-        <Typography 
+        <Typography
           variant="h5"
           component="h1"
-          sx={{ color: theme.palette.tertiary.main }}>
+          sx={{ color: theme.palette.tertiary.main }}
+        >
           Rentabilidade de {earning}% a.a.
         </Typography>
       </Box>
     </Card>
-
-
-
-  )
+  );
 }
